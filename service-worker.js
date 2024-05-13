bcf8919f611380f1f5eafb9791fecc422fcb5049
vskr <93551495+rvskr@users.noreply.github.com>
2024-05-13 17:03:03 +0300

    Update

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'links-generator-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'script.js',
+  'styles.css',
+  'manifest.json',
+  'icon.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
