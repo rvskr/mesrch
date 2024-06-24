f93696f9710a168a8728ea3fe48e247677454390
voskreseniev <roman.mayber@gmail.com>
2024-06-25 01:46:47 +0300

    +

--- service-worker.js
+++ service-worker.js
@@ -18,6 +18,21 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('activate', function(event) {
+  // Удаление старых кэшей при активации нового сервисного работника
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
@@ -31,7 +46,7 @@ self.addEventListener('fetch', function(event) {
         return fetch(event.request)
           .then(function(response) {
             // Проверяем, является ли ответ валидным
-            if(!response || response.status !== 200 || response.type !== 'basic') {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
 
@@ -45,12 +60,11 @@ self.addEventListener('fetch', function(event) {
               });
 
             return response;
-          }
-        );
-      })
-      .catch(function() {
-        // При отсутствии соединения с сетью, возвращаем ресурс из кэша
-        return caches.match(event.request);
+          })
+          .catch(function() {
+            // При ошибке загрузки из сети возвращаем ресурс из кэша
+            return caches.match(event.request);
+          });
       })
   );
 });
