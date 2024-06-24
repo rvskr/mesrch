6aeadc05c203036767da3c317beb273e4ba3a764
voskreseniev <roman.mayber@gmail.com>
2024-06-25 02:35:46 +0300

    +

--- service-worker.js
+++ service-worker.js
@@ -9,16 +9,22 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', function(event) {
+  console.log('Attempting to install service worker and cache static assets');
+  
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        return cache.addAll(urlsToCache.map(url => new Request(url, {cache: "no-store"})))
+          .then(() => console.log('Assets cached successfully'))
+          .catch(error => console.error('Failed to cache assets:', error));
       })
   );
 });
 
 self.addEventListener('activate', function(event) {
+  console.log('Service worker activating...');
+
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
@@ -33,24 +39,26 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log('Fetch event for ', event.request.url);
+
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
         if (response) {
-          return response; // Возвращаем ресурс из кэша, если он есть
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
         }
         
-        // Если ресурса нет в кэше, пытаемся загрузить его из сети
+        console.log('Network request for ', event.request.url);
         return fetch(event.request)
           .then(function(response) {
-            // Проверяем, что ответ валидный
             if (!response || response.status !== 200 || response.type !== 'basic') {
+              console.log('Response not valid, returning: ', response);
               return response;
             }
 
             var responseToCache = response.clone();
 
-            // Кэшируем загруженный ресурс
             caches.open(CACHE_NAME)
               .then(function(cache) {
                 cache.put(event.request, responseToCache);
@@ -58,8 +66,8 @@ self.addEventListener('fetch', function(event) {
 
             return response;
           })
-          .catch(function() {
-            // При ошибке загрузки из сети, возвращаем ресурс из кэша
+          .catch(function(error) {
+            console.error('Error fetching ', event.request.url, ': ', error);
             return caches.match(event.request);
           });
       })
